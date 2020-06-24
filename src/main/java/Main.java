e0fa5bad2c519f27ffbf483f953b069bf32f2166
karbudzik <karolinabudzik92@gmail.com>
2020-06-24 12:21:27 +0200

    Clear Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        String obj1 = "Blablabla";
-        String obj2 = "Mandarynka";
-        String obj3 = "Klaustrofobia";
-        String obj4 = "Klaustrofobia";
-        String obj5 = "Blablabla";
-
-        RecentlyUsedList<String> list = new RecentlyUsedList<>(3);
-        list.addItemsToList(obj1, obj2, obj3, obj4, obj5);
-        list.printItems();
+        
     }
 }
